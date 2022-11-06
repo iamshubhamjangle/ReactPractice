04037e0f513cdf7a9758b438dcec3aa0aede7785
Shubham jangle <65165548+iamshubhamjangle@users.noreply.github.com>
2022-11-06 19:36:19 +0530

    Update project15.js

--- src/project15.js
+++ src/project15.js
@@ -24,6 +24,7 @@ class Project15 extends React.Component {
         <h3>Project 15: Task 2: Infinite currying</h3>
         <pre>
           {`
+            // Method 1: ES5 function
             let add = function (a) {
                 return function (b) {
                     if(b) return fun(a+b);
@@ -31,6 +32,17 @@ class Project15 extends React.Component {
                 }
             }
 
+            // Method 2: Arrow function
+            let fun = (a) => {
+                return (b) => {
+                    return b ? fun(a+b) : a;
+                }
+            }
+            
+            // Method 3: ES6 shorthand
+            let fun = a => b => b ? fun(a+b) : a;
+            
+            // Output:
             console.log(add(2)(3)(4)(5)()); // 14
             `}
         </pre>
